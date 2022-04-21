838a6f10ffd617e594f1a77e86c34d826722824e
hidayuisaac <103895556+hidayuisaac@users.noreply.github.com>
2022-04-21 13:02:46 +0800

    Update dateFunction.php

--- dateFunction.php
+++ dateFunction.php
@@ -1,6 +1,6 @@
 <!-- HIDAYU 1912604 -->
 <?php
-    //create function with date input to convert its format
+    //create function with date input to convert its format for user's better view 
     function formatDate($bdate){
         
         $dateFormatted =date("d M Y", strtotime($bdate));
