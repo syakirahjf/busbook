d4d3cc9b988853bbc3f25c44b5cacf0342120363
syakirahjf <61780151+syakirahjf@users.noreply.github.com>
2022-04-21 11:09:07 +0800

    Update payment.php

--- payment.php
+++ payment.php
@@ -1,3 +1,4 @@
+<!--SYAKIRAH 1818436-->
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,14 +8,38 @@
     <title>BusBook</title>
     <link rel="stylesheet" href="style.css">
 </head>
-<body>
-    <div class= container> 
-    <h1 id> Your payment has been received! </h1>
-    <p id> Thank you for choosing our system. Stay safe and enjoy your journey :) </p>
+<body style="background-color: #C53A81; padding-bottom: 100px;">
+<h1 style="color:white; padding-top:50px;">BUS SEAT RESERVATION SYSTEM</h1>
+<h2 style="color:white;">FROM KUALA LUMPUR TO TERENGGANU</h2>
 
-    <form action="index.php">
-        <input id="btn" type="submit" value="Back to home">
+	<div class="container" style="background-color: white; color:black;">	
+	<h2 style="color:#FF904A;">Your booking is received!</h2>
+	<p style="text-align: left; padding: 0px 50px 0px 50px">
+	Hi,<br/>
+Thanks for your order. It’s on-hold until we confirm that payment has been received. In the meantime, here’s a reminder of what you ordered:<br/>
+
+Bank Transfer Instructions<br/><br/>
+
+1. Please make a payment using bank details below:-<br/>
+——————————–<br/>
+Bank: Maybank<br/>
+Account Number : 5512 5851 8029<br/>
+Owner: BUS BOOK SDN BHD<br/>
+Email: busbook.team@gmail.com<br/><br/>
+
+2. Send proof of your payment to:-<br/>
+——————————–<br/>
+Email: busbook.team@gmail.com, OR<br/>
+SMS/Whatsapp: 011-1076 2040<br/><br/>
+
+** Attach with “Name & Ticket ID” for our references.<br/>
+** Any Booking order valid for 24 hours<br/><br/>
+Thank you for choosing our system. Stay safe and enjoy your journey :)
+<form action="index.php">
+        <input id="btn" type="submit" value="Home" style="margin: -3000px 0 0 0; background-color: #FF904A; color:white">
     </form>
+</p>
     </div>
 </body>
+<footer id="footer">Group 2  INFO 3305 Case Study</footer>
 </html>
