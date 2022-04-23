d417dabc65f50d3b97bedfa36b4b8db60657ffb0
Amiyulism <77191588+amiyulism@users.noreply.github.com>
2022-04-23 08:53:04 +0800

    Update minMaxDate.php

--- minMaxDate.php
+++ minMaxDate.php
@@ -2,13 +2,13 @@
 <?php
     function minDate()
     {
-        return date('Y-m-d');
+        return date('Y-m-d'); //return current date
     }
 
     function maxDate()
     {
         $curDate=date('Y-m-d');
         $maxDate=date('Y-m-d', strtotime($curDate. '+10 days'));
-        return $maxDate;
+        return $maxDate; //return date 10 days after the current date
     }
 ?>
